8a21a5368a56744c1558ff42033ae5dbaad54ca4
TeamDman <TeamDman9201@gmail.com>
2014-12-15 11:35:25 -0500

    Torch polished
    Tooltips need to be added, fixed crafting and nbt persisting through
    placing and breaking.

--- src/main/java/com/TeamDman_9201/nova/Blocks/CompressedTorch.java
+++ src/main/java/com/TeamDman_9201/nova/Blocks/CompressedTorch.java
@@ -1,14 +1,23 @@
 package com.TeamDman_9201.nova.Blocks;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockTorch;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
@@ -18,7 +27,10 @@ import com.TeamDman_9201.nova.Tiles.TileEntityCompressedTorch;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class CompressedTorch extends BlockTorch {
+public class CompressedTorch extends BlockTorch implements ITileEntityProvider {
+	//Cazzar> Teamy, create another class, which extends that, and register it with the block.
+	private final Random	random	= new Random();
+
 	public CompressedTorch() {
 	}
 
@@ -60,16 +72,6 @@ public class CompressedTorch extends BlockTorch {
 	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z,
 			int metadata, int fortune) {
 		ArrayList<ItemStack> rtn = new ArrayList<ItemStack>();
-		ItemStack item = new ItemStack(NOVA.compressedTorch);
-		TileEntityCompressedTorch tile = (TileEntityCompressedTorch) world
-				.getTileEntity(x, y, z);
-		if (tile != null) {
-			System.out.println("GOT TILE");
-			NBTTagCompound compound = new NBTTagCompound();
-			compound.setInteger("Torches", tile.getTorches());
-			item.setTagCompound(compound);
-		}
-		rtn.add(item);
 		return rtn;
 	}
 
@@ -77,8 +79,7 @@ public class CompressedTorch extends BlockTorch {
 		return true;
 	}
 
-	public TileEntity createNewTileEntity(World world,int var2) {
-		System.out.println("NEW TILE WORLD " + world.toString() + "INT " + var2);
+	public TileEntity createNewTileEntity(World world, int var2) {
 		return new TileEntityCompressedTorch();
 	}
 
@@ -88,24 +89,103 @@ public class CompressedTorch extends BlockTorch {
 	public void onBlockPlacedBy(World world, int xPos, int yPos, int zPos,
 			EntityLivingBase entity, ItemStack stack) {
 		NBTTagCompound tags = stack.getTagCompound();
-		TileEntityCompressedTorch tile = (TileEntityCompressedTorch) createNewTileEntity(world,1);
-		world.setTileEntity(xPos, yPos, zPos, tile);
+		TileEntityCompressedTorch tile = (TileEntityCompressedTorch) world
+				.getTileEntity(xPos, yPos, zPos);
 		if (tags != null && tile != null) {
-			tile.writeToNBT(tags);
-		} else {
-			System.out.println("Whut no tags?");
+			tile.setTorches(tags.getInteger("Torches"));
 		}
 	}
 
-	// /**
-	// * Called after a block is placed
-	// */
-	// public void onPostBlockPlaced(World world, int xPos, int yPos, int zPos,
-	// int meta) {
-	// TileEntityCompressedTorch tile = (TileEntityCompressedTorch) world
-	// .getTileEntity(xPos, yPos, zPos);
-	// NBTTagCompound compound = new NBTTagCompound();
-	// tile.writeToNBT(compound);
-	// }
+	/**
+	 * Called upon block activation (right click on the block.)
+	 */
+	public boolean onBlockActivated(World world, int x, int y, int z,
+			EntityPlayer player, int p_149727_6_, float p_149727_7_,
+			float p_149727_8_, float p_149727_9_) {
+		if (world.isRemote) {
+			return true;
+		} else {
+			TileEntityCompressedTorch tile = (TileEntityCompressedTorch) world
+					.getTileEntity(x, y, z);
+			if (tile != null) {
+				System.out.println("Torches:" + tile.getTorches());
+			}
 
+			return true;
+		}
+	}
+
+	public void breakBlock(World world, int x, int y, int z, Block block,
+			int p_149749_6_) {
+
+		TileEntityCompressedTorch tile = (TileEntityCompressedTorch) world
+				.getTileEntity(x, y, z);
+
+		if (tile != null) {
+			ItemStack stack = new ItemStack(NOVA.compressedTorch);
+			NBTTagCompound tags = new NBTTagCompound();
+			tags.setInteger("Torches", tile.getTorches());
+			stack.setTagCompound(tags);
+			stack.setStackDisplayName("Torches Stored: " + tile.getTorches());
+
+			float f = random.nextFloat() * 0.8F + 0.1F;
+			float f1 = random.nextFloat() * 0.8F + 0.1F;
+			float f2 = random.nextFloat() * 0.8F + 0.1F;
+
+			while (stack.stackSize > 0) {
+				int j1 = random.nextInt(21) + 10;
+
+				if (j1 > stack.stackSize) {
+					j1 = stack.stackSize;
+				}
+
+				stack.stackSize -= j1;
+				EntityItem entityitem = new EntityItem(world,
+						(double) ((float) x + f), (double) ((float) y + f1),
+						(double) ((float) z + f2), new ItemStack(
+								stack.getItem(), j1, stack.getItemDamage()));
+
+				if (stack.hasTagCompound()) {
+					entityitem.getEntityItem().setTagCompound(
+							(NBTTagCompound) stack.getTagCompound().copy());
+				}
+
+				float f3 = 0.05F;
+				entityitem.motionX = (double) ((float) random.nextGaussian() * f3);
+				entityitem.motionY = (double) ((float) random.nextGaussian()
+						* f3 + 0.2F);
+				entityitem.motionZ = (double) ((float) random.nextGaussian() * f3);
+				world.spawnEntityInWorld(entityitem);
+			}
+
+			world.func_147453_f(x, y, z, block); // triggers block updates,
+													// should prob keep in
+		}
+
+		super.breakBlock(world, x, y, z, block, p_149749_6_); // removes my tile
+																// entity if
+																// isinstanceof
+																// blockContainer
+	}
+//
+//	 if (nbttagcompound.hasKey("Lore"))
+//	 {
+//	 NBTTagList nbttaglist1 = nbttagcompound.getTagList("Lore");
+//
+//	 if (nbttaglist1.tagCount() > 0)
+//	 {
+//	 for (int j = 0; j < nbttaglist1.tagCount(); ++j)
+//	 {
+//	 arraylist.add(EnumChatFormatting.DARK_PURPLE + "" + EnumChatFormatting.ITALIC + ((NBTTagString)nbttaglist1.tagAt(j)).data);
+//	 }
+//	 }
+//	 }
+////	 
+//	public void addInformation(ItemStack itemStack, EntityPlayer player,
+//			List list, boolean par4) {
+////		if (null != (itemStack.getTagCompound().getInteger("Torches")) {
+//			list.add("Torches: "+itemStack.getTagCompound().getInteger("Torches"));
+////			list.add(EnumChatFormatting.GREEN + "code: " + code);
+////		}
+//	}
 }
