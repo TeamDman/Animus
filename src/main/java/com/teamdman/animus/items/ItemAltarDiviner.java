ada13a5ed9e0c0a811cc5bb75714fd89fa82fd12
TeamDman <TeamDman9201@gmail.com>
2016-09-30 23:54:13 -0400

    Altar diviner takes inv, ignores meta Bloodrune meta isnt working

--- src/main/java/com/teamdman/animus/items/ItemAltarDiviner.java
+++ src/main/java/com/teamdman/animus/items/ItemAltarDiviner.java
@@ -6,17 +6,24 @@ import WayofTime.bloodmagic.api.altar.AltarComponent;
 import WayofTime.bloodmagic.api.altar.EnumAltarComponent;
 import WayofTime.bloodmagic.api.altar.EnumAltarTier;
 import WayofTime.bloodmagic.api.altar.IBloodAltar;
+import WayofTime.bloodmagic.block.base.BlockString;
 import WayofTime.bloodmagic.client.IVariantProvider;
+import WayofTime.bloodmagic.item.block.ItemBlockBloodRune;
+import WayofTime.bloodmagic.registry.ModBlocks;
 import WayofTime.bloodmagic.tile.TileAltar;
 import WayofTime.bloodmagic.util.ChatUtil;
 import WayofTime.bloodmagic.util.Utils;
 import com.teamdman.animus.registry.AnimusBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockStateBase;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
@@ -42,9 +49,28 @@ public class ItemAltarDiviner extends Item implements IVariantProvider {
 		tooltip.add("Keep using to automatically place altar blocks");
 		tooltip.add("Use in off hand to display max tier altar outline");
 	}
+	private boolean stackEqualExact(ItemStack stack1, ItemStack stack2)
+	{
+		if (stack1.getItem() instanceof ItemBlockBloodRune && stack2.getItem() instanceof ItemBlockBloodRune)
+			return true;
+		return stack1.getItem() == stack2.getItem() && (!stack1.getHasSubtypes() || stack1.getMetadata() == stack2.getMetadata()) && ItemStack.areItemStackTagsEqual(stack1, stack2);
+	}
+
+	private int getSlotFor(ItemStack stack, EntityPlayer player) {
+		for (int i = 0; i < player.inventory.mainInventory.length; ++i)
+		{
+			if (player.inventory.mainInventory[i] != null && stackEqualExact(stack, player.inventory.mainInventory[i]))
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
 
 	@Override
 	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		IBlockState asd = world.getBlockState(pos);
+		System.out.println(asd);
 		if (world.getTileEntity(pos) == null || !(world.getTileEntity(pos) instanceof IBloodAltar))
 			return EnumActionResult.PASS;
 		TileAltar altar = (TileAltar) world.getTileEntity(pos);
@@ -53,7 +79,6 @@ public class ItemAltarDiviner extends Item implements IVariantProvider {
 			return EnumActionResult.PASS;
 		}
 
-		//setup ghost blocks
 		for (AltarComponent altarComponent : EnumAltarTier.values()[hand.compareTo(EnumHand.OFF_HAND)==0?EnumAltarTier.MAXTIERS-1:altar.getTier().toInt()].getAltarComponents()) {
 			BlockPos componentPos = pos.add(altarComponent.getOffset());
 			BlockStack worldBlock = new BlockStack(world.getBlockState(componentPos).getBlock(), world.getBlockState(componentPos).getBlock().getMetaFromState(world.getBlockState(componentPos)));
@@ -70,11 +95,20 @@ public class ItemAltarDiviner extends Item implements IVariantProvider {
 
 			if (altarComponent.getComponent() != EnumAltarComponent.NOTAIR) {
 				if (worldBlock.getBlock() == AnimusBlocks.blockPhantomBuilder) {
-					//worldBlock.getBlock() != Utils.getBlockForComponent(altarComponent.getComponent())
-					int invSlot = player.inventory.getSlotFor(new ItemStack(Utils.getBlockForComponent(altarComponent.getComponent())));
+					int invSlot = getSlotFor(new ItemStack(Utils.getBlockForComponent(altarComponent.getComponent())), player);
 					if (invSlot != -1 || player.capabilities.isCreativeMode) {
 						if (!player.capabilities.isCreativeMode) {
-							world.setBlockState(componentPos, Block.getBlockFromItem(player.inventory.getStackInSlot(invSlot).getItem()).getDefaultState());
+							//world.setBlockState(componentPos,Block.getBlockFromItem(player.inventory.getStackInSlot(invSlot).getItem()).getDefaultState()); //Block.getBlockFromItem(player.inventory.getStackInSlot(invSlot).getItem()).getDefaultState());
+							ItemStack _stack = player.inventory.getStackInSlot(invSlot);
+							if (_stack.getItem() instanceof ItemBlock) {
+								ItemBlock _item = (ItemBlock) player.inventory.getStackInSlot(invSlot).getItem();
+								if (_item.getBlock() instanceof BlockString) {
+									System.out.println("yee");
+									IBlockState _state = ((BlockString) _item.getBlock()).getStateFromMeta(_item.getDamage(_stack));
+									world.setBlockState(componentPos, _state);
+								}
+								world.setBlockState(componentPos, _item.getBlock().getDefaultState());
+							}
 						} else {
 							world.setBlockState(componentPos, Utils.getBlockForComponent(altarComponent.getComponent()).getDefaultState());
 						}
