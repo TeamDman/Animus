591c20615f76e2a9ec8eab22935c3c1079e6cfff
Saereth <guiken@gmail.com>
2016-11-03 12:54:39 -0400

    Updated everything to MC 1.10.2, fixed orb recipes

--- src/main/java/com/teamdman/animus/registry/AnimusRecipes.java
+++ src/main/java/com/teamdman/animus/registry/AnimusRecipes.java
@@ -1,6 +1,7 @@
 package com.teamdman.animus.registry;
 
 import WayofTime.bloodmagic.api.recipe.ShapedBloodOrbRecipe;
+import WayofTime.bloodmagic.api.registry.OrbRegistry;
 import WayofTime.bloodmagic.registry.ModItems;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -20,8 +21,9 @@ public class AnimusRecipes {
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(AnimusItems.kamaGold), " a ", "a a", "  b", 'a', Items.GOLD_INGOT, 'b', Items.STICK));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(AnimusItems.kamaDiamond), " a ", "a a", "  b", 'a', Items.DIAMOND, 'b', Items.STICK));
 
-		GameRegistry.addRecipe(new ShapedBloodOrbRecipe(new ItemStack(AnimusItems.sigilChains), "aba", "bcb", "ada", 'a', Blocks.END_STONE, 'b', Blocks.REDSTONE_LAMP, 'c', new ItemStack(ModItems.slate, 1, 3), 'd', ModItems.orbMaster));
-		GameRegistry.addRecipe(new ShapedBloodOrbRecipe(new ItemStack(AnimusItems.sigilTransposition), "aba", "bcb", "ada", 'a', Blocks.OBSIDIAN, 'b', Items.ENDER_PEARL, 'c', new ItemStack(ModItems.slate, 1, 3), 'd', ModItems.orbMaster));
-		GameRegistry.addRecipe(new ShapedBloodOrbRecipe(new ItemStack(AnimusItems.sigilBuilder), "aba", "bcb", "ada", 'a', Items.SUGAR, 'b', Items.POTIONITEM, 'c',new ItemStack(ModItems.slate, 1, 3), 'd', ModItems.orbApprentice));
+		
+		GameRegistry.addRecipe(new ShapedBloodOrbRecipe(new ItemStack(AnimusItems.sigilChains), "aba", "bcb", "ada", 'a', Blocks.END_STONE, 'b', Blocks.REDSTONE_LAMP, 'c', new ItemStack(ModItems.slate, 1, 3), 'd', OrbRegistry.getOrbStack(ModItems.orbMaster)));
+		GameRegistry.addRecipe(new ShapedBloodOrbRecipe(new ItemStack(AnimusItems.sigilTransposition), "aba", "bcb", "ada", 'a', Blocks.OBSIDIAN, 'b', Items.ENDER_PEARL, 'c', new ItemStack(ModItems.slate, 1, 3), 'd', OrbRegistry.getOrbStack(ModItems.orbMaster)));
+		GameRegistry.addRecipe(new ShapedBloodOrbRecipe(new ItemStack(AnimusItems.sigilBuilder), "aba", "bcb", "ada", 'a', Items.SUGAR, 'b', Items.POTIONITEM, 'c',new ItemStack(ModItems.slate, 1, 3), 'd', OrbRegistry.getOrbStack(ModItems.orbApprentice)));
 	}
 }
