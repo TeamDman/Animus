9b971a21aa7ab051b321621b87144d2bcb7fc66a
TeamDman <TeamDman9201@gmail.com>
2016-12-10 12:47:13 -0500

    Fix #30

--- src/main/java/com/teamdman/animus/items/ItemKamaBound.java
+++ src/main/java/com/teamdman/animus/items/ItemKamaBound.java
@@ -2,6 +2,7 @@ package com.teamdman.animus.items;
 
 import WayofTime.bloodmagic.ConfigHandler;
 import WayofTime.bloodmagic.api.BloodMagicAPI;
+import WayofTime.bloodmagic.api.altar.IBloodAltar;
 import WayofTime.bloodmagic.api.util.helper.PlayerSacrificeHelper;
 import WayofTime.bloodmagic.item.ItemDaggerOfSacrifice;
 import net.minecraft.entity.Entity;
@@ -10,8 +11,12 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -38,17 +43,28 @@ public class ItemKamaBound extends ItemDaggerOfSacrifice {
 
 
 	@Override
+	public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World world, EntityPlayer player, EnumHand hand) {
+		checkAndKill(player.posX,player.posY,player.posZ, world, player,true);
+		return new ActionResult<>(EnumActionResult.SUCCESS,stack);
+	}
+
+	@Override
 	public boolean hitEntity(ItemStack stack, EntityLivingBase hit, EntityLivingBase attacker) {
 		double x = hit.posX;
 		double y = hit.posY;
 		double z = hit.posZ;
+		checkAndKill(x,y,z,hit.world,attacker,false);
+		return false;
+	}
+
+	private void checkAndKill(double x, double y, double z, World world, EntityLivingBase attacker, boolean efficient) {
 		int d0 = 10;
 		AxisAlignedBB region = new AxisAlignedBB(x, y, z, x, y, z).expand(d0, d0, d0);
-		List<EntityLivingBase> entities = hit.world.getEntitiesWithinAABB(EntityLivingBase.class, region);
+		List<EntityLivingBase> entities = world.getEntitiesWithinAABB(EntityLivingBase.class, region);
 		if (entities == null || entities.isEmpty())
-			return false;
+			return;
 		for (EntityLivingBase target : entities) {
-//			System.out.println(target.getDisplayName());
+			//			System.out.println(target.getDisplayName());
 			if (target == null || attacker == null || attacker.world.isRemote || (attacker instanceof EntityPlayer && !(attacker instanceof EntityPlayerMP)))
 				continue;
 
@@ -75,12 +91,24 @@ public class ItemKamaBound extends ItemDaggerOfSacrifice {
 			if (target.isChild())
 				lifeEssence /= 2;
 
-			if (PlayerSacrificeHelper.findAndFillAltar(attacker.world, attacker, lifeEssence, true)) {
+			if (efficient?findAndFillAltar(world,attacker,lifeEssence):PlayerSacrificeHelper.findAndFillAltar(world, attacker, lifeEssence, true)) {
 				target.world.playSound(null, target.posX, target.posY, target.posZ, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 0.5F, 2.6F + (target.world.rand.nextFloat() - target.world.rand.nextFloat()) * 0.8F);
 				target.setHealth(-1);
 				target.onDeath(BloodMagicAPI.getDamageSource());
 			}
 		}
-		return false;
+	}
+
+	private boolean findAndFillAltar(World world, EntityLivingBase sacrificingEntity, int amount) {
+		IBloodAltar altarEntity = PlayerSacrificeHelper.getAltar(world, sacrificingEntity.getPosition());
+		if (altarEntity == null)
+			return false;
+		if (altarEntity.getCurrentBlood()+amount > altarEntity.getCapacity())
+			return false;
+
+		altarEntity.sacrificialDaggerCall(amount, true);
+		altarEntity.startCycle();
+
+		return true;
 	}
 }
