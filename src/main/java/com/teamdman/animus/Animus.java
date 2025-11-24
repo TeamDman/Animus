8fb308e43f3d7f50d4fb255404663df33b8c694a
Saereth <paulanthonydevs@gmail.com>
2025-11-24 13:54:28 -0800

    Registers config during mod initialization.
    Registers the mod's configuration during the initialization phase.
    This ensures that configuration options are available early in the mod's lifecycle.

--- src/main/java/com/teamdman/animus/Animus.java
+++ src/main/java/com/teamdman/animus/Animus.java
@@ -2,8 +2,8 @@ package com.teamdman.animus;
 
 import com.teamdman.animus.registry.*;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,9 +12,11 @@ public class Animus {
     public static final Logger LOGGER = LogManager.getLogger();
     public static Animus instance;
 
-    public Animus() {
+    public Animus(IEventBus modEventBus, ModLoadingContext modLoadingContext) {
         instance = this;
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register config
+        AnimusConfig.register(modLoadingContext);
 
         // Register all deferred registers
         AnimusBlocks.BLOCKS.register(modEventBus);
