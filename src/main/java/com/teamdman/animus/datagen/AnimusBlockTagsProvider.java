dd8e97770540a5d310105c4a88c3fa49e65426f3
Saereth <paulanthonydevs@gmail.com>
2025-12-01 22:45:48 -0800

    fence datagen so they actually connect...

--- src/main/java/com/teamdman/animus/datagen/AnimusBlockTagsProvider.java
+++ src/main/java/com/teamdman/animus/datagen/AnimusBlockTagsProvider.java
@@ -36,11 +36,35 @@ public class AnimusBlockTagsProvider extends BlockTagsProvider {
         this.tag(BlockTags.SAPLINGS)
             .add(AnimusBlocks.BLOCK_BLOOD_SAPLING.get());
 
+        // WOODEN FENCES TAG
+        this.tag(BlockTags.WOODEN_FENCES)
+            .add(AnimusBlocks.BLOCK_BLOOD_WOOD_FENCE.get());
+
+        // FENCES TAG (for connection logic)
+        this.tag(BlockTags.FENCES)
+            .add(AnimusBlocks.BLOCK_BLOOD_WOOD_FENCE.get());
+
+        // FENCE GATES TAG
+        this.tag(BlockTags.FENCE_GATES)
+            .add(AnimusBlocks.BLOCK_BLOOD_WOOD_FENCE_GATE.get());
+
+        // STAIRS TAG
+        this.tag(BlockTags.STAIRS)
+            .add(AnimusBlocks.BLOCK_BLOOD_WOOD_STAIRS.get());
+
+        // SLABS TAG
+        this.tag(BlockTags.SLABS)
+            .add(AnimusBlocks.BLOCK_BLOOD_WOOD_SLAB.get());
+
         // MINEABLE WITH AXE - Wood blocks and core
         this.tag(BlockTags.MINEABLE_WITH_AXE)
             .add(AnimusBlocks.BLOCK_BLOOD_WOOD.get())
             .add(AnimusBlocks.BLOCK_BLOOD_WOOD_STRIPPED.get())
             .add(AnimusBlocks.BLOCK_BLOOD_WOOD_PLANKS.get())
+            .add(AnimusBlocks.BLOCK_BLOOD_WOOD_STAIRS.get())
+            .add(AnimusBlocks.BLOCK_BLOOD_WOOD_SLAB.get())
+            .add(AnimusBlocks.BLOCK_BLOOD_WOOD_FENCE.get())
+            .add(AnimusBlocks.BLOCK_BLOOD_WOOD_FENCE_GATE.get())
             .add(AnimusBlocks.BLOCK_BLOOD_CORE.get());
 
         // MINEABLE WITH HOE - Leaves
